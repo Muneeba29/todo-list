030884f6340decc9d509175c7561161352d44d88
Muneeba Barkat Ali <ibnehawwa29@gmail.com>
2024-11-06 23:21:08 +0500

    Add files via upload

---
+++ to-do list.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("inputbox");
+const ulList = document.getElementById("list");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        ulList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+ulList.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", ulList.innerHTML);
+}
+
+function showTask() {
+    ulList.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
